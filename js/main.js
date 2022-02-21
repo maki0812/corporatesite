43018c8884572084ff5c85ee257102649918c9f0
maki0812 <samnel2180@yahoo.co.jp>
2022-02-21 13:08:45 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+//ハンバーガーメニュー
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
+  const nav = document.getElementById("drawer-nav");
+  nav.classList.toggle('in');
 });
+
+//問い合わせフォーム
+const contact = document.getElementByName("form1")[1], {
+  function check(){
+    if (name_form.name.value == "") {
+      alert("このフィールドに入力してください。");
+      return false;
+    } else {
+      return true;
+    }
+  }
